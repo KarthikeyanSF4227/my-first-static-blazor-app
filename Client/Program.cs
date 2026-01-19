b627b080b9ecfe96c92475a34c059c35b161298a
KarthikeyanSF4227 <karthikeyan.palanisamy@syncfusion.cm>
2026-01-19 19:43:27 +0530

    Added the simple grid sample

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,12 @@
+using BlazorApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorApp.Client;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
